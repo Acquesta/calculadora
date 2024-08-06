f49ee39127fb05d24ee6a835342c119697e38f09
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2024-08-06 11:45:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
 const display = document.querySelector('#display')
+const subDisplay = document.querySelector('#subDisplay')
 const igual = document.querySelector('#igual')
+const mais = document.querySelector('#mais')
+const menos = document.querySelector('#menos')
+const C = document.querySelector('#C')
 
-igual.addEventListener('click', () =>{
-    display.innerHTML = `Ola mundo`
+igual.addEventListener('click', () => {
+    var soma = parseInt(display.innerHTML);
+    for (let i = 0; i < n1.length; i++) {
+        soma += n1[i]
+    }
+    display.innerHTML = `${soma}`
+    subDisplay.innerHTML = ''
+    n1 = []
 })
 
 document.addEventListener('click', (e) => {
-    console.log(e);
-    if(e.target.id != '' && e.target.){
-        display.innerHTML = `${e.target.id}`
+    // console.log(e);
+    if (e.target.id != '' && e.target.id != 'igual' && e.target.id != 'mais' && e.target.id != 'C' && e.target.id != 'menos') {
+        if (display.innerHTML == 'DISPLAY') {
+            display.innerHTML = ''
+            subDisplay.innerHTML = ''
+        }
+        display.innerHTML += `${e.target.id}`
     }
+})
+
+let n1 = [];
+
+mais.addEventListener('click', () => {
+
+    if (display.innerHTML != '') {
+
+        n1.push(parseInt(display.innerHTML))
+        display.innerHTML = ''
+        subDisplay.innerHTML = ''
+        for (let i = 0; i < n1.length; i++) {
+            subDisplay.innerHTML += ` ${n1[i]} +`
+        }
+    }
+})
+
+C.addEventListener('click', () => {
+    display.innerHTML = ''
+})
+
+menos.addEventListener('click', () => {
+    display.innerHTML = '-  '
 })
